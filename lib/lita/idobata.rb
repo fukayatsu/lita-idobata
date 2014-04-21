23bf549d8b188cc45fa77cc58c485607e7c61b5e
fukayatsu <fukayatsu@gmail.com>
2014-04-21 21:58:08 +0900

    first commit

---
+++ lib/lita/idobata.rb
@@ -0,0 +1,7 @@
+require "lita/idobata/version"
+
+module Lita
+  module Idobata
+    # Your code goes here...
+  end
+end
---
+++ lib/lita/idobata/version.rb
@@ -0,0 +1,5 @@
+module Lita
+  module Idobata
+    VERSION = "0.0.1"
+  end
+end
